9cdcd52889331a9cfc71a2f4e93d6ebb0e8debd4
Tu Vo <tuvo1106@gmail.com>
2020-02-26 21:34:08 -0800

    Add AccountStatus imports in all people classes

--- parking_lot/people/account.py
+++ parking_lot/people/account.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from parking_lot.data_types.enums import AccountStatus
+
 
 class Account:
-    def __init__(self, user_name, password, person, status=AccountStatus.Active):
+    def __init__(self, user_name, password, person, status=AccountStatus.ACTIVE):
         self.__user_name = user_name
         self.__password = password
         self.__person = person
--- parking_lot/people/parking_attendant.py
+++ parking_lot/people/parking_attendant.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+from parking_lot.data_types.enums import AccountStatus
 from parking_lot.people.account import Account
 
 
 class ParkingAttendant(Account):
-    def __init__(self, user_name, password, person, status=AccountStatus.Active):
+    def __init__(self, user_name, password, person, status=AccountStatus.ACTIVE):
         super().__init__(user_name, password, person, status)
 
     def process_ticket(self, ticket_number):
