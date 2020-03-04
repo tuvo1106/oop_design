0b1aab83132537cacd0ed359060402aedc195df6
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-03-04 08:46:11 -0800

    Add blackjack card classes

---
+++ blackjack/card/blackjack_card.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from blackjack.card.card import Card
+
+
+class BlackjackCard(Card):
+    def __init__(self, suit, face_value):
+        super().__init__(suit, face_value)
+        self.__game_value = face_value
+        if self.__game_value > 10:
+            self.__game_value = 10
+
+    def get_game_value(self):
+        return self.__game_value
