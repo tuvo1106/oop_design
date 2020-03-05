91d6f8442f35bf9d2c8aeccf1d8aa3214340f111
Tu Vo <tuvo1106@gmail.com>
2020-03-05 11:20:18 -0800

    Add hotel hotel classes

---
+++ hotel/hotel/hotel.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+class Hotel:
+    def __init__(self, name):
+        self.__name = name
+        self.__locations = []
+
+    def add_location(self, location):
+        pass
---
+++ hotel/hotel/hotel_location.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+class HotelLocation:
+    def __init__(self, name, address):
+        self.__name = name
+        self.__location = address
+
+    def get_rooms(self):
+        pass
