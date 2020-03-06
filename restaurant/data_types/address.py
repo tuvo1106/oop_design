b9cb0b1e17f219d0b9925333df239fb7885332f5
Tu Vo <tuvo1106@gmail.com>
2020-03-06 13:09:05 -0800

    Add restaurant data type classes

---
+++ restaurant/data_types/address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, street, city, state, zip_code, country):
+        self.__street_address = street
+        self.__city = city
+        self.__state = state
+        self.__zip_code = zip_code
+        self.__country = country
---
+++ restaurant/data_types/enums.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+
+class ReservationStatus(Enum):
+    REQUESTED = 1
+    PENDING = 2
+    CONFIRMED = 3
+    CHECKED_IN = 4
+    CANCELED = 5
+    ABANDONED = 6
+
+
+class SeatType(Enum):
+    REGULAR, KID, ACCESSIBLE, OTHER = 1, 2, 3, 4
+
+
+class OrderStatus(Enum):
+    RECEIVED, PREPARING, COMPLETED, CANCELED, NONE = 1, 2, 3, 4, 5
+
+
+class TableStatus(Enum):
+    FREE, RESERVED, OCCUPIED, OTHER = 1, 2, 3, 4
+
+
+class AccountStatus(Enum):
+    ACTIVE, CLOSED, CANCELED, BLACKLISTED, BLOCKED = 1, 2, 3, 4, 5
+
+
+class PaymentStatus(Enum):
+    UNPAID = 1
+    PENDING = 2
+    COMPLETED = 3
+    FILLED = 4
+    DECLINED = 5
+    CANCELLED = 6
+    ABANDONED = 7
+    SETTLING = 8
+    SETTLED = 9
+    REFUNDED = 10
