aa9c6bb2c63513492b0ddd1607d3a9774f4a4242
Tu Vo <tuvo1106@gmail.com>
2020-03-16 08:42:14 -0700

    Add linkedin profile classes

---
+++ linkedin/profile/experience.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+class Experience:
+    def __init__(self, title, company, location, date_from, date_to,
+                 description):
+        self.__title = title
+        self.__company = company
+        self.__location = location
+        self.__from = date_from
+        self.__to = date_to
+        self.__description = description
---
+++ linkedin/profile/profile.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+class Profile:
+    def __init__(self, summary, experiences, educations, skills,
+                 accomplishments, recommendations):
+        self.__summary = summary
+        self.__experiences = experiences
+        self.__educations = educations
+        self.__skills = skills
+        self.__accomplishments = accomplishments
+        self.__recommendations = recommendations
+        self.__stats = []
+
+    def add_experience(self, experience):
+        pass
+
+    def add_education(self, education):
+        pass
+
+    def add_skill(self, skill):
+        pass
+
+    def add_accomplishment(self, accomplishment):
+        pass
+
+    def add_recommendation(self, recommendation):
+        pass
