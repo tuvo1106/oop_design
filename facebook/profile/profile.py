041881d4a50a33c49693fabeca5c9b2fbffab90a
Tu Vo <tuvo1106@gmail.com>
2020-03-18 09:19:50 -0700

    Add facebook profile classes

---
+++ facebook/profile/profile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+class Profile:
+    def __init__(self, profile_picture, cover_photo, gender):
+        self.__profile_picture = profile_picture
+        self.__cover_photo = cover_photo
+        self.__gender = gender
+        self.__work_experiences = []
+        self.__educations = []
+        self.__places = []
+        self.__stats = []
+
+    def add_work_experience(self, work):
+        pass
+
+    def add_education(self, education):
+        pass
+
+    def add_place(self, place):
+        pass
